15b334dc29e3c09c7172932e6539c3ae4a02a0c1
SAC-CS112-Mejia-John <john.andrew.mejia@gmail.com>
2015-10-07 21:17:59 -0700

    Changed ifs to switches

--- src/MainClass.java
+++ src/MainClass.java
@@ -11,78 +11,100 @@ public class MainClass {
 		Random myRNG = new Random();
 		Scanner myKeyboard = new Scanner(System.in);
 		String doAgain;
-		
+		int cpuChoice = myRNG.nextInt(3);
+
 		do {
-			
+
 			String myChoice = JOptionPane.showInputDialog(null, "Please enter S for scissors, R for rock, or P for paper: ").toUpperCase();
 
 
-			if ((!(myChoice.equals("R"))) && (!(myChoice.equals("P"))) && (!(myChoice.equals("S")))) 
+			while ((!(myChoice.equals("R"))) && (!(myChoice.equals("P"))) && (!(myChoice.equals("S")))) 
 			{
 				JOptionPane.showMessageDialog(null, "Error: Invalid choice.");			
 			}
-			else{
+			switch(myChoice.toUpperCase()){
 
-				int cpuChoice = myRNG.nextInt(3);
-				if (myChoice.equals("R")) 
+
+			case "R": 
+			{
+				switch(cpuChoice) 
 				{
+				case 0: 
+				{
+					JOptionPane.showMessageDialog(null, "We both picked rock!\nThe game is a draw.");
+					break;
 
-					if (cpuChoice == 0) 
-					{
-						JOptionPane.showMessageDialog(null, "We both picked rock!\nThe game is a draw.");
-
-					} 
-					else if (cpuChoice == 1) 
-					{
-						JOptionPane.showMessageDialog(null, "You picked rock.\nI picked paper.\nYou lose!");
-
-					} 
-					else if (cpuChoice == 2) 
-					{
-						JOptionPane.showMessageDialog(null, "You picked rock.\nI picked scissors.\nYou win!");
-					}
 				} 
-				else if (myChoice.equals("P")) 
+				case 1: 
 				{
-					if (cpuChoice == 0) 
-					{
-						JOptionPane.showMessageDialog(null, "You picked paper.\nI picked rock.\nYou win!");
-					} 
-					else if (cpuChoice == 1) 
-					{
-						JOptionPane.showMessageDialog(null, "We both picked paper!\nThe game is a draw.");
-					} 
-					else if (cpuChoice == 2)
-					{
-						JOptionPane.showMessageDialog(null, "You picked paper.\nI picked scissors.\nYou lose!");
-					}
+					JOptionPane.showMessageDialog(null, "You picked rock.\nI picked paper.\nYou lose!");
+					break;
+
 				} 
-				else if (myChoice.equals("S")) 
+				case 2: 
 				{
-					if (cpuChoice == 0) 
-					{
-						JOptionPane.showMessageDialog(null, "You picked scissors.\nI picked rock.\nYou lose!");
-					} 
-					else if (cpuChoice == 1) 
-					{
-						JOptionPane.showMessageDialog(null, "You picked scissors.\nI picked paper.\nYou win!");
-					} 
-					else if (cpuChoice == 2) 
-					{
-						JOptionPane.showMessageDialog(null, "We both picked scissors!\nThe game is a draw.");
-					}
+					JOptionPane.showMessageDialog(null, "You picked rock.\nI picked scissors.\nYou win!");
+					break;
 				}
 
+				}
+				break;
+			} 
+			case "P": 
+			{
+				switch(cpuChoice)
+				{
+				case 0: 
+				{
+					JOptionPane.showMessageDialog(null, "You picked paper.\nI picked rock.\nYou win!");
+					break;
+				} 
+				case 1: 
+				{
+					JOptionPane.showMessageDialog(null, "We both picked paper!\nThe game is a draw.");
+					break;
+				} 
+				case 2: 
+				{
+					JOptionPane.showMessageDialog(null, "You picked paper.\nI picked scissors.\nYou lose!");
+					break;
+				}
+
+				}
+				break;
+			} 
+			case "S":
+				switch(cpuChoice)
+				{
+				case 0: 
+				{
+					JOptionPane.showMessageDialog(null, "You picked scissors.\nI picked rock.\nYou lose!");
+					break;
+				} 
+				case 1: 
+				{
+					JOptionPane.showMessageDialog(null, "You picked scissors.\nI picked paper.\nYou win!");
+					break;
+				} 
+				case 2: 
+				{
+					JOptionPane.showMessageDialog(null, "We both picked scissors!\nThe game is a draw.");
+					break;
+				}
+
+				}
+				break;
+
 			}
-			
+
 			doAgain = JOptionPane.showInputDialog("Do it again? (Y/N)").toUpperCase();
 			while(!(doAgain.matches("[YN]+"))) 
 			{				
 				JOptionPane.showMessageDialog(null, "Error: Invalid choice.");
 				doAgain = JOptionPane.showInputDialog("Do it again? (Y/N)").toUpperCase();
-				
+
 			}
-			
+
 		} while (!(doAgain.equals("N")));
 		JOptionPane.showMessageDialog(null,"GoodBye!");
 	}
